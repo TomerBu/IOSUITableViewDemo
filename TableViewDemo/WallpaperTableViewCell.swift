ce86ead9412a43db5a57bf1c75f8d8563d679ee7
tomerbuzaglo <tomer.bu@gmail.com>
2016-04-12 14:53:30 +0300

    Added Custom Edit Actions and animations. also in the comments added some filters. Finally-> Added a blur view instead

--- TableViewDemo/WallpaperTableViewCell.swift
+++ TableViewDemo/WallpaperTableViewCell.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class WallpaperTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var seasonImageView: UIImageView!
+
+    @IBOutlet weak var paperTitleLabel: UILabel!
+    
+    @IBOutlet weak var paperSubTitleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- TableViewDemo/WallpaperTableViewController.swift
+++ TableViewDemo/WallpaperTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class WallpaperTableViewController: UITableViewController {
     
+    
     var dataSource = DataSource()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +27,9 @@ class WallpaperTableViewController: UITableViewController {
         self.refreshControl = UIRefreshControl()
         refreshControl?.attributedTitle = NSAttributedString(string: "Pull To Refresh!")
         self.refreshControl = UIRefreshControl()
- 
+        
         self.tableView.addSubview(self.refreshControl!) // not required when using UITableViewController
-
+        
         self.refreshControl?.addTarget(self, action: #selector(refresh), forControlEvents: UIControlEvents.ValueChanged)
     }
     
@@ -55,7 +56,7 @@ class WallpaperTableViewController: UITableViewController {
         self.tableView.reloadData()
         refreshControl.endRefreshing()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -82,8 +83,51 @@ class WallpaperTableViewController: UITableViewController {
         
         if let wallPaper = dataSource.wallPaperForRowAtIndexPath(indexPath){
             cell.seasonImageView.image = wallPaper.image
+            cell.paperTitleLabel.text = wallPaper.title
+            cell.paperSubTitleLabel.text = wallPaper.subTitle
+            
+            //cell.backgroundImageView.image = wallPaper.image
+            
+            UIView.transitionWithView(cell.backgroundImageView, duration: 1.3, options: .TransitionCurlUp, animations: {
+                cell.backgroundImageView.image = wallPaper.image
+                }, completion: nil)
+            
+            
+            
+            
+            
+            //            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), {
+            //
+            //                let image = wallPaper.image
+            //                let filter = CIFilter(name: "CIAreaAverage")! //"CIBoxBlur","CIColumnAverage", "CIAreaAverage", "CICrystallize", "CIComicEffect", "CIPhotoEffectChrome", "CIPhotoEffectTonal", "CICircularWrap" ,"CIPhotoEffectTransfer", "CISepiaTone", "CIVignette", "CIVignetteEffect"
+            //                let cgImage = CIImage(image: image)
+            //                filter.setValue(cgImage, forKey: kCIInputImageKey)
+            //                let cgimg:CGImage = CIContext().createCGImage(filter.outputImage!, fromRect: filter.outputImage!.extent)
+            //
+            //                let processedImage = UIImage(CGImage: cgimg)
+            //                dispatch_async(dispatch_get_main_queue(), {
+            //
+            //                    UIView.transitionWithView(cell.backgroundImageView, duration: 0.5, options: .TransitionCrossDissolve, animations: {
+            //                         cell.backgroundImageView.image = processedImage
+            //                        }, completion: nil)
+            //
+            //                })
+            //            })
         }
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+        let btnKill = UITableViewRowAction(style: .Default, title: "Kill!", handler: { (action, indexPath) in
+            self.tableView(tableView, commitEditingStyle: UITableViewCellEditingStyle.Delete, forRowAtIndexPath: indexPath)
+        })
+        btnKill.backgroundColor = UIColor.redColor()
+        let btnKeep = UITableViewRowAction(style: .Default, title: "Keep!", handler: { (action, indexPath) in
+            print("Keeper :)")
+            tableView.editing = false
+        })
+        btnKeep.backgroundColor = UIColor.greenColor()
+        return [btnKill, btnKeep]
     }
     
     override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
